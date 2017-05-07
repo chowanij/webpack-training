92c8b327a6ed6dcfacc8a64a09dd6a79a20c5104
chowanij <jan.chowaniec@outlook.com>
2017-05-07 21:12:01 +0200

    initial commit

---
+++ api.js
@@ -0,0 +1,22 @@
+module.exports = {
+    getUsers: function() {
+        return[
+            {
+                name: 'Jan',
+                age: '32'
+            },
+            {
+                name: 'Alex',
+                age: '8'    
+            },
+            {
+                name: 'Magulon',
+                age: '23'
+            },
+            {
+                name: 'dupa',
+                age: '56'
+            },
+        ];
+    }
+};
